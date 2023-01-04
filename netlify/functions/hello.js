b798ceb0a0f4b689b01159293ca9c45edc8671a1
dhiamonkey <dhiamuhammad@gmail.com>
2023-01-04 15:15:55 +0700

    fix type coercion

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,11 @@
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+console.log("Connected to PlanetScale!");
+
 exports.handler = function (event, context, callback) {
   // Get the value to increment from the request body
-  const value = JSON.parse(event.body).value;
+  const value = +JSON.parse(event.body).value;
 
   // Increment the value by 1
   const incrementedValue = value + 1;
@@ -12,3 +17,10 @@ exports.handler = function (event, context, callback) {
   };
   callback(null, response);
 };
+
+// database: first_database
+// username: 6dwmgmsbpa7wlw0vlm20
+// host: ap-southeast.connect.psdb.cloud
+// password: pscale_pw_GnqaAms8Kb4bk5wOduI2FlXTr0USODmZiDqGiYL1ESF
+
+// mysql://6dwmgmsbpa7wlw0vlm20:pscale_pw_GnqaAms8Kb4bk5wOduI2FlXTr0USODmZiDqGiYL1ESF@ap-southeast.connect.psdb.cloud/first_database?ssl={"rejectUnauthorized":true}
