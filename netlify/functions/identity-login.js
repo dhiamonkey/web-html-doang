6e8740414222e2f8d4ad82ebcef9ec6da47e8539
dhiamonkey <dhiamuhammad@gmail.com>
2023-01-09 11:12:16 +0700

    add response

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -3,4 +3,8 @@ exports.handler = async function (event, context) {
   // Do stuff and return a response...
   console.log(identity);
   console.log(user);
+  const response = {
+    statusCode: 200,
+  };
+  callback(null, response);
 };
